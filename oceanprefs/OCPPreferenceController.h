4f9a879277c476c98b9daed9da7e617bd149423d
Tommy Muir <31448720+Muirey03@users.noreply.github.com>
2019-01-13 13:07:42 +0000

    Add a banner

--- oceanprefs/OCPPreferenceController.h
+++ oceanprefs/OCPPreferenceController.h
@@ -1,5 +1,14 @@
 #import <CSPrefs/libCSPreferences.h>
 
+@interface UIImage (Internal)
++(id)imageNamed:(id)arg1 inBundle:(id)arg2;
+@end
+
 @interface OCPPreferenceController : CSPListController
 
+@end
+
+@interface OCPPrefBannerCell : UITableViewCell {
+    UIImageView* imgView;
+}
 @end
